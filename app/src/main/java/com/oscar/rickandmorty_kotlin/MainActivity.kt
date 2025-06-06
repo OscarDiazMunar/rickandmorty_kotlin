76587e46ec43350da6a31f267915e84f922db3ac
oscar diaz <oscar.diaz.munar@gmail.com>
2025-06-06 18:11:51 -0500

    initial commit

---
+++ app/src/main/java/com/oscar/rickandmorty_kotlin/MainActivity.kt
@@ -0,0 +1,46 @@
+package com.oscar.rickandmorty_kotlin
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.oscar.rickandmorty_kotlin.ui.theme.Rickandmorty_kotlinTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            Rickandmorty_kotlinTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                    Greeting("Android")
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    Rickandmorty_kotlinTheme {
+        Greeting("Android")
+    }
+}
